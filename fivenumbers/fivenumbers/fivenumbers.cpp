b7496f4a40e688702fe7ef76b509cdee0341ab7b
Merrell Gilbert <sam17me@yahoo.com>
2018-02-06 13:35:20 -0600

    almost done adding all the code.

--- fivenumbers/fivenumbers/fivenumbers.cpp
+++ fivenumbers/fivenumbers/fivenumbers.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
+/* ask the user for five numbers return the sum and the average of the numbers */
 
+double a, b, c, d, e, f;
 void main()
 {
+	cout << " Please enter the first number: ";
+	cin >> a;
+	cout << endl << " Please enter the second number: ";
+	cin << endl << " Please enter the third number: "; 
 
 
 }
